2a7c6ca2fffc91f23b13afecf05ffdafd1c1b0c4
Prain Lopez <qym200042@gmail.com>
2023-04-14 18:45:11 +0000

    +teleRecv.js 解决JSON解析

--- teleRecv.js
+++ teleRecv.js
@@ -4,11 +4,13 @@ const secretToken = "Prainy_here";
 
 const teleWebhook = express();
 
+teleWebhook.use(express.json())
+
 teleWebhook.get('/', (req, res) => {  // res need to be removed.
     console.log(req.body);
     res.send('pong!'); //to be removed
 });
 
 teleWebhook.listen(port, () => {
-    console.log('Listening Telegram Webhook on port %d.', port);
+    console.log(`Listening Telegram Webhook on port ${port}.`);
 });
