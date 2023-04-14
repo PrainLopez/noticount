a1fb73042330bace6a739c17002e6010496baf6c
Prain Lopez <qym200042@gmail.com>
2023-04-14 15:19:55 +0000

    +nodemon(dev) +debug scripts

--- teleRecv.js
+++ teleRecv.js
@@ -3,3 +3,11 @@ const port = 16913;
 const secretToken = "Prainy_here";
 
 const teleWebhook = express();
+
+teleWebhook.get('/', (req, res) => {  // res need to be removed.
+    console.log(req.body);
+});
+
+teleWebhook.listen(port, () => {
+    console.log('Listening Telegram Webhook on port ${port}.');
+});
