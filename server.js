82300ac931e5a76d49d19125363fdca838b8b3b6
Prain Lopez <qym200042@gmail.com>
2023-05-24 11:25:24 +0000

    完成telegramAPI提取消息

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ function errGen(status, msg){
 
 // Authenticate secrest token
 teleWebhook.use('/', function(req, res, next) {
-    console.log('Authenticating...') // TODO: 测试用
     //check telegram secretToken
     let token = req.get('x-telegram-bot-api-secret-token');
     //key empty
     if (!token) return next(errGen(400, 'token required'));
     //key invalid
     if (token != config.secretToken) return next(errGen(401, 'invalid token'))
+    console.log('token verified'); // TODO: 测试用，应删除
 
     // 需要进一步鉴权：
     next();
@@ -30,8 +30,20 @@ teleWebhook.use(express.json())
 
 // Receive message from Telegram
 teleWebhook.use('/', function (req, res) {
-    msg = req.body.message;
-    res.send(msg); // TODO: 测试用
+    msg = {};
+    
+    if (req.body.message.chat.type == 'private') {
+        msg.upd_id = req.body.update_id;
+        msg.user_id = req.body.message.from.id;
+        msg.user_name = req.body.message.from.username;
+        msg.time = req.body.message.date;
+        msg.text = req.body.message.text;
+    }
+    else{
+        next(errGen(400, 'not private chat'));
+    }
+    res.send(msg);
+    
     });
 
 teleWebhook.listen(config.port, () => 
