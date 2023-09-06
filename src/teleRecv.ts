0e421bc4dd886fb794a59e947f9a56d77a563ce7
Prain Lopez <61592725+PrainLopez@users.noreply.github.com>
2023-09-06 09:13:49 +0000

    add comments

--- src/teleRecv.ts
+++ src/teleRecv.ts
@@ -1,8 +1,10 @@
 // 输入经过鉴权的telegram bot发来的JSON实例，输出记账文本（字符串）
 
+// 用于接受http传来的自然语言记账文本
 interface rawLanguageIn {
     text: String;
     recvTime: number;
 
+    //监听方法
     receive(): Promise<String>;
 }
