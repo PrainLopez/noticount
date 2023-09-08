c86452308f62efd951ba88f8a32f67e2bbae3106
Prain Lopez <qym200042@gmail.com>
2023-09-08 17:05:14 +0800

    problem log

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,20 @@ interface LangInReq extends Request {
 }
 
 listener.use('/recv', (req: LangInReq, res: Response, next: NextFunction) => {
+/* 
+src/server.ts:10:23 - error TS2769: No overload matches this call.
+  The last overload gave the following error.
+    Argument of type '(req: LangInReq, res: Response, next: NextFunction) => void' is not assignable to parameter of type 'Application<Record<string, any>>'.
+      Type '(req: LangInReq, res: Response<any, Record<string, any>>, next: NextFunction) => void' is missing the following properties from type 'Application<Record<string, any>>': init, defaultConfiguration, engine, set, and 63 more.
+
+10 listener.use('/recv', (req: LangInReq, res: Response, next: NextFunction) => {
+                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  node_modules/@types/express-serve-static-core/index.d.ts:185:5
+    185     (path: PathParams, subApplication: Application): T;
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    The last overload is declared here.
+*/
     req.recvTime = Date.now();
     
     let token = req.get('x-telegram-bot-api-secret-token');
