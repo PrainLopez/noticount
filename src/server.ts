3142708b11f137a2d74c4c23bd0a0557ec72b74e
PrainLopez <qym200042@gmail.com>
2023-09-09 19:30:32 +0800

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,25 @@ import config from '../config.json' assert {type: "json"};
 const listener = express();
 
 listener.use('/recv', (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore
-    req.local.recvTime = Date.now();
+  let recvTime: number = Date.now();
+  // @ts-ignore
+  req.locals = req.locals || {};
+  // @ts-ignore
+  req.locals.recvTime = recvTime;
     
-    let token = req.get('x-telegram-bot-api-secret-token');
-    if(token !== config.telegramBotListener.botToken) {
-        console.log(`[WARN] Unauthorized request from ${req.ip} with token ${token}`);
-        return;
-    }
+  let token = req.get('x-telegram-bot-api-secret-token');
+  if(token !== config.telegramBotListener.botToken) {
+    console.log(`[WARN] Unauthorized request from ${req.ip} with token ${token}`);
+    return;
+  }
 
-    next();
+  next();
 });
 
 listener.use(express.json());
 
 listener.use('/recv', (req, res, next) => {
-    
+    console.log(`[INFO] Received request from ${req.ip} with Authorized telegram bot token`);
 });
 
 listener.listen(config.port, () => {
