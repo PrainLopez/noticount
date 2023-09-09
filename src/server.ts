f57ff58ffc28884a3656cfe5e07b451fc02a9e76
PrainLopez <qym200042@gmail.com>
2023-09-09 15:19:22 +0800

    fix it with @ts-ignore

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,12 @@ import config from '../config.json' assert {type: "json"};
 
 const listener = express();
 
-interface LangInReq extends Request {
-    recvTime: number;
-}
-
-listener.use('/recv', (req: LangInReq, res: Response, next: NextFunction) => {
-/* 
-src/server.ts:10:23 - error TS2769: No overload matches this call.
-  The last overload gave the following error.
-    Argument of type '(req: LangInReq, res: Response, next: NextFunction) => void' is not assignable to parameter of type 'Application<Record<string, any>>'.
-      Type '(req: LangInReq, res: Response<any, Record<string, any>>, next: NextFunction) => void' is missing the following properties from type 'Application<Record<string, any>>': init, defaultConfiguration, engine, set, and 63 more.
-
-10 listener.use('/recv', (req: LangInReq, res: Response, next: NextFunction) => {
-                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  node_modules/@types/express-serve-static-core/index.d.ts:185:5
-    185     (path: PathParams, subApplication: Application): T;
-            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    The last overload is declared here.
-
-    https://stackoverflow.com/questions/71243903/accessing-middleware-injected-data-in-typescript
-*/
-    req.recvTime = Date.now();
+listener.use('/recv', (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    req.local.recvTime = Date.now();
     
     let token = req.get('x-telegram-bot-api-secret-token');
-    if(token !== config.telegram.botToken) {
+    if(token !== config.telegramBotListener.botToken) {
         console.log(`[WARN] Unauthorized request from ${req.ip} with token ${token}`);
         return;
     }
