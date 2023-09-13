18a13ca45b75ca334d412b788cf501100249e3dd
Prain Lopez <qym200042@gmail.com>
2023-09-13 17:31:54 +0800

    update receive module

--- src/receive.ts
+++ src/receive.ts
@@ -1,14 +1,14 @@
 // 输入经过鉴权的telegram bot发来的JSON实例，输出记账文本（字符串）
 
 // 用于接受http传来的自然语言记账文本
-import express, {Express, Request, Response} from "express";
+import { Request } from "express";
 
-interface RawLanguageIn {
+interface AccountingSession {
     text: String;
     recvTime: number;
 
     
 
     //监听方法
-    receive(): Promise<String>;
+    extract(req: Request): Promise<AccountingSession>;
 }
