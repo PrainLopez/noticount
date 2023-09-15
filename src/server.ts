d698377304b7a8a00116513b395c9b1bc66f0bd8
PrainLopez <qym200042@gmail.com>
2023-09-16 00:20:20 +0800

    building server.js

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import config from '../config.json' assert {type: "json"};
 interface AbstractAccountingSession {
   naturalLanguageText: string;
   inTime: number;
+
+  process(): Promise<AbstractAccountingSession>;
 }
 
 const listener = express();
@@ -11,10 +13,24 @@ const listener = express();
 listener.use(express.json());
 
 listener.use('/recv', (req: Request, res: Response, next: NextFunction) => {
-  if (config.telegramBotListener.enable == true 
-    || config.telegramBotListener.botToken == req.headers['x-telegram-bot-token']) {
+  const recvTime = Date.now();
 
+  if (config.telegramBotListener.enable == true 
+    || config.telegramBotListener.botToken == req.get('x-telegram-bot-api-secret-token')) {
       
+      class RecvTelegramBotSession implements AbstractAccountingSession {
+        private naturalLanguageText: string;
+        private inTime: number;
+
+        constructor(res: Response, recvTime: number) {
+          this.naturalLanguageText = naturalLanguageText;
+          this.inTime = recvTime;
+        }
+
+        async process(): Promise<AbstractAccountingSession> {
+          return this;
+        }
+      }
     }
 
   next();
