13bf7807f7f2a31205f27c9db88b87e2597057ee
PrainLopez <qym200042@gmail.com>
2023-09-17 12:14:39 +0800

    formatted

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,61 @@
-import express, {Express, NextFunction, Request, Response} from "express";
-import config from '../config.json' assert {type: "json"};
+import express, { Express, NextFunction, Request, Response } from 'express'
+import config from '../config.json' assert { type: 'json' }
 
 interface AbstractAccountingSession {
-  naturalLanguageText: string | undefined;
-  inTime: number | undefined;
+  naturalLanguageText: string | undefined
+  inTime: number | undefined
 
-  process(): Promise<AbstractAccountingSession>;
+  process(): Promise<AbstractAccountingSession>
 }
 
-const listener = express();
+const listener = express()
 
-listener.use(express.json());
+listener.use(express.json())
 
 listener.use('/recv', (req: Request, res: Response, next: NextFunction) => {
-  const recvTime = Date.now();
+  const recvTime = Date.now()
 
-  if (config.telegramBotListener.enable == true 
-    || config.telegramBotListener.botToken == req.get('X-Telegram-Bot-Api-Secret-Token')) {
+  if (
+    config.telegramBotListener.enable == true ||
+    config.telegramBotListener.botToken ==
+      req.get('X-Telegram-Bot-Api-Secret-Token')
+  ) {
+    // TODO: Implement further authorization
 
-      // TODO: Implement further authorization
-      
-      class RecvTelegramBotSession implements AbstractAccountingSession {
-        naturalLanguageText: string | undefined;
-        inTime: number | undefined;
+    class RecvTelegramBotSession implements AbstractAccountingSession {
+      naturalLanguageText: string | undefined
+      inTime: number | undefined
 
-        constructor(req: Request, recvTime: number) {
-          this.naturalLanguageText = req.body.message.text;
-          this.inTime = recvTime;
-        }
-
-        async process(): Promise<AbstractAccountingSession> {
-          return new Promise((resolve, reject) => {
-            try {
-              resolve(this);
-            } catch (error) {
-              reject(error);
-            }
-          });
-        }
+      constructor (req: Request, recvTime: number) {
+        this.naturalLanguageText = req.body.message.text
+        this.inTime = recvTime
       }
 
-      const session = new RecvTelegramBotSession(req, recvTime);
-      session.process()
-        .then((session) => {
-          // TODO: Next process
+      async process (): Promise<AbstractAccountingSession> {
+        return new Promise((resolve, reject) => {
+          try {
+            resolve(this)
+          } catch (error) {
+            reject(error)
+          }
         })
-        .catch((error) => {
-          // TODO: Handle error
-        });
+      }
     }
 
-  next();
-});
+    const session = new RecvTelegramBotSession(req, recvTime)
+    session
+      .process()
+      .then(session => {
+        // TODO: Next process
+      })
+      .catch(error => {
+        // TODO: Handle error
+      })
+  }
+
+  next()
+})
 
 listener.listen(config.port, () => {
-  console.log(`[INFO] Listening on port ${config.port}`);
-});
+  console.log(`[INFO] Listening on port ${config.port}`)
+})
