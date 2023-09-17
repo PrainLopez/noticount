540a41c08a1ee0e70032d55fa99bfd1277ba50d2
PrainLopez <qym200042@gmail.com>
2023-09-17 18:49:55 +0800

    building llmParse.ts

--- src/llmParse.ts
+++ src/llmParse.ts
@@ -3,3 +3,27 @@ import config from "../config.json" assert { type: "json" };
 
 import OpenAI from "openai";
 
+if (config.parser.chatGPT.enable) {
+    const openai = new OpenAI({
+        apiKey: config.parser.chatGPT.apiKey,
+      });
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            "role": "system",
+            "content": config.parser.chatGPT.system_message
+          },
+          {
+            "role": "user",
+            "content": "傻逼百事要我4块钱"
+          }
+        ],
+        temperature: config.parser.chatGPT.temperature,
+        max_tokens: config.parser.chatGPT.max_tokens,
+        top_p: config.parser.chatGPT.top_p,
+        frequency_penalty: config.parser.chatGPT.frequency_penalty,
+        presence_penalty: config.parser.chatGPT.presence_penalty,
+      });
+}
