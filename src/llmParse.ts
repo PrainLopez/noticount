7e16d2948d8770b114e1faf19f50fcbd0c4b5f18
Prain Lopez <qym200042@gmail.com>
2023-09-19 14:25:38 +0800

    building llmParse.ts

--- src/llmParse.ts
+++ src/llmParse.ts
@@ -1,31 +1,51 @@
-import { AbstractAccountingSession } from '../types/interfaces.js'
-import config from '../config.json' assert { type: 'json' }
+import { AbstractAccountingSession } from "../types/interfaces.js";
+import config from "../config.json" assert { type: "json" };
+import OpenAI from "openai";
 
-import OpenAI from 'openai'
-
-async function gptParse (session: AbstractAccountingSession) {
-  if (config.parser.chatGPT.enable) {
-    const openai = new OpenAI({
-      apiKey: config.parser.chatGPT.apiKey
-    })
-
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content: config.parser.chatGPT.system_message
-        },
-        {
-          role: 'user',
-          content: session.naturalLanguageText
-        }
-      ],
-      temperature: config.parser.chatGPT.temperature,
-      max_tokens: config.parser.chatGPT.max_tokens,
-      top_p: config.parser.chatGPT.top_p,
-      frequency_penalty: config.parser.chatGPT.frequency_penalty,
-      presence_penalty: config.parser.chatGPT.presence_penalty
-    })
+export async function parser(session: AbstractAccountingSession): Promise<AbstractAccountingSession> {
+  if (session.naturalLanguageText == null) {
+    throw new Error("Natural language text is null");
   }
+  
+}
+
+class ChatGPTParser implements AbstractAccountingSession{
+  naturalLanguageText: string | null;
+  inTime: number | null;
+  recordEvent: string | null;
+  recordAmount: number | null;
+  nextParser: AbstractAccountingSession | null;
+
+  constructor(session: AbstractAccountingSession, nextParser: AbstractAccountingSession | null = null){
+    this.naturalLanguageText = session.naturalLanguageText;
+    this.inTime = session.inTime;
+    this.recordEvent = session.recordEvent;
+    this.recordAmount = session.recordAmount;
+    this.nextParser = nextParser;
+  }
+
+  async process(): Promise<AbstractAccountingSession>{
+  const openai = new OpenAI({
+    apiKey: config.parser.chatGPT.apiKey,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: config.parser.chatGPT.system_message,
+      },
+      {
+        role: "user",
+        content: session.naturalLanguageText,
+      },
+    ],
+    temperature: config.parser.chatGPT.temperature,
+    max_tokens: config.parser.chatGPT.max_tokens,
+    top_p: config.parser.chatGPT.top_p,
+    frequency_penalty: config.parser.chatGPT.frequency_penalty,
+    presence_penalty: config.parser.chatGPT.presence_penalty,
+  });
+}
 }
