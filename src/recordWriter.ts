d6b5e0b001d761695907324ddf995c9484b73e3a
Prain Lopez <qym200042@gmail.com>
2023-09-21 17:31:25 +0800

    building recordWriter.ts

--- src/recordWriter.ts
+++ src/recordWriter.ts
@@ -2,4 +2,24 @@ import { AbstractAccountingSession } from "../types/interfaces.js";
 
 import { config } from "process";
 
-export function writer
+abstract class AbstractWriter implements AbstractAccountingSession {
+    naturalLanguageText: string | null
+    inTime: number | null
+    recordEvent: string | null
+    recordAmount: number | null
+    abstract nextWriter: AbstractAccountingSession | null
+    
+    constructor(
+        session: AbstractAccountingSession,
+        nextWriter: AbstractAccountingSession | null = null
+    ) {
+        this.naturalLanguageText = session.naturalLanguageText
+        this.inTime = session.inTime
+        this.recordEvent = session.recordEvent
+        this.recordAmount = session.recordAmount
+    }
+    
+    abstract process(): Promise<AbstractAccountingSession>;
+}
+
+export function writer (session: AbstractAccountingSession): void {}
