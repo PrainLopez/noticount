8532bd7019b754e22cacce1891dc69d4c089a21a
PrainLopez <qym200042@gmail.com>
2024-12-18 16:48:21 +0000

    kickstart the server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
 import { Hono } from 'hono'
+import { bearerAuth } from "hono/bearer-auth";
+
+const token: string = process.env.BEARER_TOKEN as string;
+console.log("token", token)
+
+interface record {
+  index: number;
+  amount: number;
+  remark?: string | null;
+  timestamp: number;
+}
+
+const bunFile = Bun.file('data/prainy-account.json', { type: "application/json" })
+let table: Array<record>
+if (await bunFile.exists()) {
+  table = await bunFile.json()
+} else {
+  table = new Array<record>()
+}
 
 const app = new Hono()
+
+// '/'
+app.use('/', bearerAuth({ token }))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+app.post('/v1.0/Prainy/account', async (c) => {
+  try {
+    const record: record = await c.req.json()
+  } catch (err) {
+    console.log(`Error while retrieving record: ${err}`)
+  }
+
+
+})
+
+export default {
+  port: 3681,
+  fetch: app.fetch,
+}
