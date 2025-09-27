b3b135e7f186d929e7255a2666d8a83aaf51f231
Prainy <qym200042@gmail.com>
2025-09-27 22:48:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Noticount
+
+This is a simple self-hosted, multi currency accounting application.
+
+---
+
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    react: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      "indent": 2,
+      "semi": true,
+      "quotes": "double",
+      "react/jsx-indent": [2, 2],
+      "trailingComma": "never",
+    },
+  },
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "style/jsx-one-expression-per-line": ["off"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfigRootDir: ".",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+    },
+  },
+);
